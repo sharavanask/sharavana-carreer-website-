507aafaae5526de21f405aa9401561729dacad6b
sharavanask <sharavanakrishna2004@gmail.com>
2024-10-03 18:20:19 +0530

    Added dynamic data

---
+++ 1.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template,jsonify
+app=Flask(__name__)
+
+
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Scientist',
+        'location': 'Delhi, India',
+        'salary': 'Rs 15,00,000'
+    },
+    {
+        'id': 2,
+        'title': 'Machine Learning Engineer',
+        'location': 'Bangalore, India',
+        'salary': 'Rs 18,00,000'
+    },
+    {
+        'id': 3,
+        'title': 'Software Engineer',
+        'location': 'Hyderabad, India',
+        
+    },
+    {
+        'id': 4,
+        'title': 'AI Researcher',
+        'location': 'Chennai, India',
+        'salary': 'Rs 20,00,000'
+    }
+]
+
+
+@app.route('/')
+def run():
+    return render_template('home.html',jobs=JOBS)
+
+@app.route('/api/jobs')
+def listsharavana():
+    return jsonify(JOBS)
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',debug=True)
